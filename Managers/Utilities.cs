f570ecc861cf415e9a06b4f5c195c3fd782b81aa
fionabarton <56236345+fionabarton@users.noreply.github.com>
2023-01-16 18:23:35 -0500

    Add SetPosition(GameObject tGO, float x, float y)

--- Managers/Utilities.cs
+++ Managers/Utilities.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 // A set of general functions that are HOPEFULLY useful in a multitude of projects
 public class Utilities : MonoBehaviour {
+    // Set GameObject Position
+    public void SetPosition(GameObject tGO, float x, float y) {
+        Vector3 tPos = tGO.transform.position;
+        tPos.x = x;
+        tPos.y = y;
+        tGO.transform.position = tPos;
+    }
+
     // Activate or deactivate all of the gameObject elements stored within a list 
     public void SetActiveList(List<GameObject> objects, bool isActive) {
         for (int i = 0; i < objects.Count; i++) {
