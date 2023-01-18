6d46342b99626b308bebc8667f2560334182e681
fionabarton <56236345+fionabarton@users.noreply.github.com>
2023-01-17 21:58:12 -0500

    Add ResetUserPosition.cs
    Add ResetUserPosition, which resets the user's position if they've fallen through the floor.

---
+++ Managers/ResetUserPosition.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Resets the user's position if they've fallen through the floor
+public class ResetUserPosition : MonoBehaviour {
+    [Header("Set in Inspector")]
+    public Transform            resetPosition;
+    [SerializeField] GameObject xrOriginGO;
+    //[SerializeField] Camera     playerHead;
+
+    public Animator             faderAnim;
+
+    public void ResetPosition() {
+        faderAnim.CrossFade("Fadeout", 0);
+
+        var distanceDiff = resetPosition.position - Camera.main.transform.position;
+        xrOriginGO.transform.position += distanceDiff;
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.tag == "Player") {
+            ResetPosition();
+        }
+    }
+}
