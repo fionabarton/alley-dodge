895e07f4de6ec36257ced224daa7fc9574f91111
fionabarton <56236345+fionabarton@users.noreply.github.com>
2023-01-18 21:36:15 -0500

    Add ShadowFollow.cs
    Add ShadowFollow, which makes the player's shadows follow its body and hands.

---
+++ UserVisuals/ShadowFollow.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Makes the player's shadows follow its body and hands 
+public class ShadowFollow : MonoBehaviour {
+    [Header("Set in Inspector")]
+    public Transform objectToFollow;
+
+    private void Start() {
+        StartCoroutine("Follow");
+    }
+
+    IEnumerator Follow() {
+        Vector3 tPos = transform.position;
+        tPos = new Vector3(objectToFollow.position.x, 0.01f, objectToFollow.position.z);
+        transform.position = tPos;
+
+        yield return null;
+        StartCoroutine("Follow");
+    }
+}
