a7693d6594d28bf9f91e28e5abdd49e2c6b82bab
fionabarton <56236345+fionabarton@users.noreply.github.com>
2023-01-20 18:20:00 -0500

    Add WarningManager.cs
    Add WarningManager.cs, which on trigger press, deactivate the disclaimer warning the user to not jump and also be wary of their surroundings as the playspace can shift during gameplay.

---
+++ UserVisuals/WarningManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+// On trigger press, deactivate the disclaimer warning the user to not jump and also
+// be wary of their surroundings as the playspace can shift during gameplay
+public class WarningManager : MonoBehaviour {
+    [Header("Set in Inspector")]
+    public InputActionReference toggleReference = null;
+
+    void Awake() {
+        toggleReference.action.started += DeactivateWarning;
+    }
+
+    void OnDestroy() {
+        toggleReference.action.started -= DeactivateWarning;
+    }
+
+    void DeactivateWarning(InputAction.CallbackContext context) {
+        Destroy(gameObject);
+    }
+}
