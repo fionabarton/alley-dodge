0a9269a59dc342a5fa185b284ebf634ba0b14bab
fionabarton <56236345+fionabarton@users.noreply.github.com>
2023-02-23 19:17:42 -0500

    Tune particle system instantiation position
    Instantiate pickup particle systems at the midpoint between the pickup and player.

--- InteractableObjects/ShieldPickup.cs
+++ InteractableObjects/ShieldPickup.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-//
+// Handles what happens after the player has collided with a shield pickup
 public class ShieldPickup : MonoBehaviour {
     [Header("Set in Inspector")]
     public GameObject   explosionGO;
@@ -10,7 +10,15 @@ public class ShieldPickup : MonoBehaviour {
     private void OnTriggerEnter(Collider other) {
         if (other.gameObject.tag == "Player") {
             // Instantiate particle system
-            Instantiate(explosionGO, transform.position, transform.rotation);
+            SphereCollider sphereCollider = other.GetComponent<SphereCollider>();
+            if (sphereCollider) { // Collided with hand
+                Instantiate(explosionGO, GameManager.utilities.GetMidpoint(gameObject, other.gameObject), transform.rotation);
+            } else { // Collided with body
+                float xPos = other.transform.position.x + (gameObject.transform.position.x - other.transform.position.x) / 2;
+                float zPos = other.transform.position.z + (gameObject.transform.position.z - other.transform.position.z) / 2;
+                Vector3 midPoint = new Vector3(xPos, transform.position.y, zPos);
+                Instantiate(explosionGO, midPoint, transform.rotation);
+            }
 
             // SFX
             GameManager.audioMan.PlayPlayerSFXClip(eSFX.sfxBuff2);
