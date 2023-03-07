c23bca5e463db86124e28cfe70d70bd033eb6eee
fionabarton <56236345+fionabarton@users.noreply.github.com>
2023-03-06 19:45:16 -0500

    ObjectSpawner.cs: Add object & spawn speed fields

--- InteractableObjects/MoveObject.cs
+++ InteractableObjects/MoveObject.cs
@@ -4,12 +4,11 @@ using UnityEngine;
 
 // Move an object in a single, specified direction at a specified speed.
 public class MoveObject : MonoBehaviour {
-    [SerializeField] float      speed = 5;
     [SerializeField] Vector3    direction = Vector3.forward;
 
     void FixedUpdate() {
         if (GameManager.S.spawner.objectsCanMove) {
-            transform.Translate(direction * Time.fixedDeltaTime * speed);
+            transform.Translate(direction * Time.fixedDeltaTime * GameManager.S.spawner.objectSpeed);
         }
     }
 }
