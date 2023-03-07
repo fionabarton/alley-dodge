bf4e9efcad6daaefbb568181406b94de1c093b5f
fionabarton <56236345+fionabarton@users.noreply.github.com>
2023-03-07 18:36:47 -0500

    Add bug fix
    - Bug: On collision with the user, OnTriggerEnter() is occasionally called twice.
    - Fix: Add ‘isTriggered’ boolean to ensure OnTriggerEnter() is called only once.

--- InteractableObjects/QuidPickup.cs
+++ InteractableObjects/QuidPickup.cs
@@ -7,27 +7,34 @@ public class QuidPickup : MonoBehaviour {
     [Header("Set in Inspector")]
     public GameObject   explosionGO;
 
+    [Header("Set Dynamically")]
+    bool                isTriggered;
+
     private void OnTriggerEnter(Collider other) {
         if (other.gameObject.tag == "Player") {
-            // Increase score;
-            GameManager.S.score.AddToScore(1);
+            if (!isTriggered) {
+                isTriggered = true;
 
-            // Instantiate particle system
-            SphereCollider sphereCollider = other.GetComponent<SphereCollider>();
-            if (sphereCollider) { // Collided with hand
-                Instantiate(explosionGO, GameManager.utilities.GetMidpoint(gameObject, other.gameObject), transform.rotation);
-            } else { // Collided with body
-                float xPos = other.transform.position.x + (gameObject.transform.position.x - other.transform.position.x) / 2;
-                float zPos = other.transform.position.z + (gameObject.transform.position.z - other.transform.position.z) / 2;
-                Vector3 midPoint = new Vector3(xPos, transform.position.y, zPos);
-                Instantiate(explosionGO, midPoint, transform.rotation);
+                // Increase score;
+                GameManager.S.score.AddToScore(1);
+
+                // Instantiate particle system
+                SphereCollider sphereCollider = other.GetComponent<SphereCollider>();
+                if (sphereCollider) { // Collided with hand
+                    Instantiate(explosionGO, GameManager.utilities.GetMidpoint(gameObject, other.gameObject), transform.rotation);
+                } else { // Collided with body
+                    float xPos = other.transform.position.x + (gameObject.transform.position.x - other.transform.position.x) / 2;
+                    float zPos = other.transform.position.z + (gameObject.transform.position.z - other.transform.position.z) / 2;
+                    Vector3 midPoint = new Vector3(xPos, transform.position.y, zPos);
+                    Instantiate(explosionGO, midPoint, transform.rotation);
+                }
+
+                // SFX
+                GameManager.audioMan.PlayPlayerSFXClip(eSFX.sfxConfirm);
+
+                // Destroy this GO
+                Destroy(gameObject);
             }
-
-            // SFX
-            GameManager.audioMan.PlayPlayerSFXClip(eSFX.sfxConfirm);
-
-            // Destroy this GO
-            Destroy(gameObject);
         }
     }
 }
--- InteractableObjects/ShieldPickup.cs
+++ InteractableObjects/ShieldPickup.cs
@@ -7,30 +7,37 @@ public class ShieldPickup : MonoBehaviour {
     [Header("Set in Inspector")]
     public GameObject   explosionGO;
 
+    [Header("Set Dynamically")]
+    bool                isTriggered;
+
     private void OnTriggerEnter(Collider other) {
         if (other.gameObject.tag == "Player") {
-            // Instantiate particle system
-            SphereCollider sphereCollider = other.GetComponent<SphereCollider>();
-            if (sphereCollider) { // Collided with hand
-                Instantiate(explosionGO, GameManager.utilities.GetMidpoint(gameObject, other.gameObject), transform.rotation);
-            } else { // Collided with body
-                float xPos = other.transform.position.x + (gameObject.transform.position.x - other.transform.position.x) / 2;
-                float zPos = other.transform.position.z + (gameObject.transform.position.z - other.transform.position.z) / 2;
-                Vector3 midPoint = new Vector3(xPos, transform.position.y, zPos);
-                Instantiate(explosionGO, midPoint, transform.rotation);
+            if (!isTriggered) {
+                isTriggered = true;
+
+                // Instantiate particle system
+                SphereCollider sphereCollider = other.GetComponent<SphereCollider>();
+                if (sphereCollider) { // Collided with hand
+                    Instantiate(explosionGO, GameManager.utilities.GetMidpoint(gameObject, other.gameObject), transform.rotation);
+                } else { // Collided with body
+                    float xPos = other.transform.position.x + (gameObject.transform.position.x - other.transform.position.x) / 2;
+                    float zPos = other.transform.position.z + (gameObject.transform.position.z - other.transform.position.z) / 2;
+                    Vector3 midPoint = new Vector3(xPos, transform.position.y, zPos);
+                    Instantiate(explosionGO, midPoint, transform.rotation);
+                }
+
+                // SFX
+                GameManager.audioMan.PlayPlayerSFXClip(eSFX.sfxBuff2);
+
+                // Activate shield
+                GameManager.shield.SetActiveShield(true);
+
+                // Display text
+                GameManager.S.score.SetDisplayText("SHIELD!", Color.blue, Color.blue);
+
+                // Destroy this GO
+                Destroy(gameObject);
             }
-
-            // SFX
-            GameManager.audioMan.PlayPlayerSFXClip(eSFX.sfxBuff2);
-
-            // Activate shield
-            GameManager.shield.SetActiveShield(true);
-
-            // Display text
-            GameManager.S.score.SetDisplayText("SHIELD!", Color.blue, Color.blue);
-
-            // Destroy this GO
-            Destroy(gameObject);
         }
     }
 }
