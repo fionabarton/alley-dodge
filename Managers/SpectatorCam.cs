b0b1dde47e6a96ad46b94cb1886a8e13b3f076da
fionabarton <56236345+fionabarton@users.noreply.github.com>
2023-03-12 21:49:08 -0400

    Ensure UI text is on top layer of overlay

--- Managers/SpectatorCam.cs
+++ Managers/SpectatorCam.cs
@@ -12,6 +12,8 @@ public class SpectatorCam : MonoBehaviour {
     public Transform                targetTrans;
     public List<Transform>          transforms; // 0: (Room > ObjectSpawner), 1: (Room > Handles > Row1 > Handle(2))
 
+    public List<GameObject>         gameObjectsInvisibleToHMD;
+
     [Header("Set Dynamically")]
     private Camera                  cam;
 
@@ -40,7 +42,7 @@ public class SpectatorCam : MonoBehaviour {
         // Set target display by its display index
         cam.targetDisplay = 0;
 
-        //
+        // Display text
         displayText.text = "Display: <color=white>Look at Spawner</color>";
 
         // Set transform to look at
@@ -48,13 +50,15 @@ public class SpectatorCam : MonoBehaviour {
     }
 
     void Update() {
-        float x = Input.GetAxis("Horizontal");
-        float y = Input.GetAxis("Vertical");
+        if (currentDisplayNdx != 2) {
+            float x = Input.GetAxis("Horizontal");
+            float y = Input.GetAxis("Vertical");
 
-        // Move camera
-        Vector3 movement = new Vector3(x, y, 0);
-        movement = Vector3.ClampMagnitude(movement, 1);
-        transform.Translate(movement * speed * Time.fixedDeltaTime);
+            // Move camera
+            Vector3 movement = new Vector3(x, y, 0);
+            movement = Vector3.ClampMagnitude(movement, 1);
+            transform.Translate(movement * speed * Time.fixedDeltaTime);
+        }
 
         // Switch monitor's target display
         if (Input.GetKeyDown(KeyCode.V)) {
@@ -76,8 +80,11 @@ public class SpectatorCam : MonoBehaviour {
 
             // Set display properties
             if (currentDisplayNdx == 0) {
-                cam.targetDisplay = 0;
-                displayText.text = "Display: <color=white>Look at Spawner</color>";
+                // Set parent transform
+                gameObject.transform.SetParent(null);
+
+                // Make objects visible to spectator cam
+                SetObjectsLayer("Invisible to HMD");
 
                 // Set transform to look at
                 targetTrans = transforms[0];
@@ -85,48 +92,77 @@ public class SpectatorCam : MonoBehaviour {
                 // Set transform position & rotation to cached values
                 gameObject.transform.position = currentTransformPositions[0];
                 gameObject.transform.rotation = currentTransformRotations[0];
-            } else if (currentDisplayNdx == 1) {
-                cam.targetDisplay = 0;
-                displayText.text = "Display: <color=white>Look at Player</color>";
 
+                // Display text
+                displayText.text = "Display: <color=white>Look at Spawner</color>";
+            } else if (currentDisplayNdx == 1) {
                 // Set transform to look at
                 targetTrans = transforms[1];
+
+                // Make objects visible to spectator cam
+                SetObjectsLayer("Invisible to HMD");
 
                 // Set transform position & rotation to cached values
                 gameObject.transform.position = currentTransformPositions[1];
                 gameObject.transform.rotation = currentTransformRotations[1];
+
+                // Display text
+                displayText.text = "Display: <color=white>Look at Player</color>";
             } else {
-                cam.targetDisplay = 1;
+                // Set parent transform
+                gameObject.transform.SetParent(Camera.main.transform);
+
+                // Make objects invisible to spectator cam
+                SetObjectsLayer("Invisible to Spectator Cam");
+
+                // Set position and rotation to 0
+                gameObject.transform.localPosition = Vector3.zero;
+                gameObject.transform.localRotation = Quaternion.identity;
+
+                // Display text
                 displayText.text = "Display: <color=white>Player HMD</color>";
             }
         }
 
-        // Reset transform position & rotation to their default values
-        if (Input.GetKeyDown(KeyCode.R)) {
-            if (currentDisplayNdx == 0) {
-                gameObject.transform.position = defaultTransformPositions[0];
-                gameObject.transform.rotation = defaultTransformRotations[0];
-            } else if (currentDisplayNdx == 1) {
-                gameObject.transform.position = defaultTransformPositions[1];
-                gameObject.transform.rotation = defaultTransformRotations[1];
+        if (currentDisplayNdx != 2) {
+            // Reset transform position & rotation to their default values
+            if (Input.GetKeyDown(KeyCode.R)) {
+                if (currentDisplayNdx == 0) {
+                    gameObject.transform.position = defaultTransformPositions[0];
+                    gameObject.transform.rotation = defaultTransformRotations[0];
+                } else if (currentDisplayNdx == 1) {
+                    gameObject.transform.position = defaultTransformPositions[1];
+                    gameObject.transform.rotation = defaultTransformRotations[1];
+                }
             }
-        }
 
-        // Zoom in
-        if(Vector3.Distance(targetTrans.position, transform.position) >= 7.5f) {
-            if (Input.GetButton("CamForward")) {
-                transform.Translate(Vector3.forward * Time.fixedDeltaTime * speed);
+            // Zoom in
+            if (Vector3.Distance(targetTrans.position, transform.position) >= 7.5f) {
+                if (Input.GetButton("CamForward")) {
+                    transform.Translate(Vector3.forward * Time.fixedDeltaTime * speed);
+                }
             }
-        }
 
-        // Zoom out
-        if (Input.GetButton("CamBackward")) {
-            transform.Translate(Vector3.back * Time.fixedDeltaTime * speed);
+            // Zoom out
+            if (Input.GetButton("CamBackward")) {
+                transform.Translate(Vector3.back * Time.fixedDeltaTime * speed);
+            }
+        } 
+    }
+
+    // Make objects (in)visible to spectator cam
+    void SetObjectsLayer(string layerName) {
+        int layerNdx = LayerMask.NameToLayer(layerName);
+        gameObject.layer = layerNdx;
+        for (int i = 0; i < gameObjectsInvisibleToHMD.Count; i++) {
+            gameObjectsInvisibleToHMD[i].layer = layerNdx;
         }
     }
 
     private void LateUpdate() {
-        // Aim camera at target
-        transform.LookAt(targetTrans);
+        if (currentDisplayNdx != 2) {
+            // Aim camera at target
+            transform.LookAt(targetTrans);
+        }
     }
 }
