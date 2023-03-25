a13320a6514a040f77537af8c69f04672651385e
fionabarton <56236345+fionabarton@users.noreply.github.com>
2023-03-24 21:24:49 -0400

    Add DisplaySelectedDropdownOptionFirst.cs
    Add DisplaySelectedDropdownOptionFirst, which on dropdown value changed, display its currently selected option first.

---
+++ UI Menus/DisplaySelectedDropdownOptionFirst.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// On dropdown value changed, display its currently selected option first
+public class DisplaySelectedDropdownOptionFirst : MonoBehaviour {
+    [Header("Set in Inspector")]
+    public TMPro.TMP_Dropdown   dropdown;
+
+    public RectTransform        dropdownContentRT;
+    public RectTransform        dropdownItemRT;
+
+    void Start() {
+        // Add listener to dropdown
+        dropdown.onValueChanged.AddListener(delegate { SetDropdownContentPosition(); });
+    }
+
+    // Adjust the dropdown content's anchored position to display its currently selected option first
+    void SetDropdownContentPosition() {
+        dropdownContentRT.anchoredPosition = new Vector2(dropdownContentRT.anchoredPosition.x,
+                                                         dropdownItemRT.sizeDelta.y * dropdown.value);
+    }
+}
