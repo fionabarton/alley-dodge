65879dc7d3c3a9d7da293674cb49dbe13eb1566e
fionabarton <56236345+fionabarton@users.noreply.github.com>
2023-03-28 16:58:56 -0400

    On new high score, play fireworks particle system

--- Managers/ConfettiManager.cs
+++ Managers/ConfettiManager.cs
@@ -4,16 +4,14 @@ using UnityEngine;
 
 public class ConfettiManager : MonoBehaviour {
     [Header("Set in Inspector")]
-    public ParticleSystem confettiPS1;
-    public ParticleSystem confettiPS2;
-    public List<ParticleSystem> particleSystems;
+    public List<ParticleSystem> confettiPS;
 
-    // Plays both particle systems that drop confetti for approximately 3 seconds
-    public void DropConfetti(bool usePlayerAudioSource = true) {
-        //confettiPS1.Play();
-        //confettiPS2.Play();
-        for (int i = 0; i < particleSystems.Count; i++) {
-            particleSystems[i].Play();
+    public ParticleSystem       fireworksPS;
+
+    // Plays particle systems that drop confetti for approximately 3 seconds
+    public void DropConfetti(bool usePlayerAudioSource = true, bool playFireworks = false) {
+        for (int i = 0; i < confettiPS.Count; i++) {
+            confettiPS[i].Play();
         }
 
         // Play SFX
@@ -22,5 +20,21 @@ public class ConfettiManager : MonoBehaviour {
         } else {
             GameManager.audioMan.PlayUISFXClip(eSFX.sfxApplause);
         }
+
+        // Play fireworks
+        if (playFireworks) {
+            fireworksPS.Play();
+        }
+    }
+
+    // Sets whether all particle systems are looping or not
+    public void IsLooping(bool isLooping = false) {
+        for (int i = 0; i < confettiPS.Count; i++) {
+            var confettiMain = confettiPS[i].main;
+            confettiMain.loop = isLooping;
+        }
+
+        var fireworksMain = fireworksPS.main;
+        fireworksMain.loop = isLooping;
     }
 }
