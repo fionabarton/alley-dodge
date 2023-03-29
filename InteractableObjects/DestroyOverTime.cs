a6221de6270c0a8fd16698dc2050be3e983302b9
fionabarton <56236345+fionabarton@users.noreply.github.com>
2023-03-29 14:00:46 -0400

    Add (un)pause timer

--- InteractableObjects/DestroyOverTime.cs
+++ InteractableObjects/DestroyOverTime.cs
@@ -4,9 +4,52 @@ using UnityEngine;
 
 // Destroys this object within a specified amount of seconds
 public class DestroyOverTime : MonoBehaviour {
-    [SerializeField] float seconds;
+	[Header("Set in Inspector")]
+	public bool		destroyRatherThanSetActive;
+	public float	timeDuration = 1f;
 
-    void Start() {
-        Destroy(gameObject, seconds);
-    }
+	[Header("Set Dynamically")]
+	private float	timePaused;
+	private float	timeDone;
+
+	private bool	timeIsPaused;
+
+	void OnEnable() {
+		timeDone = timeDuration + Time.time;
+
+		StartCoroutine("FixedUpdateCoroutine");
+	}
+
+	public IEnumerator FixedUpdateCoroutine() {
+        if (!timeIsPaused) {
+			if (timeDone <= Time.time) {
+				if (destroyRatherThanSetActive) {
+					Destroy(gameObject);
+				} else {
+					gameObject.SetActive(false);
+				}
+			}
+		} 
+
+		yield return new WaitForFixedUpdate();
+		StartCoroutine("FixedUpdateCoroutine");
+	}
+
+	//
+	public void PauseTimer() {
+		// Cache timePaused
+		timePaused = Time.time;
+
+		// Pause timer
+		timeIsPaused = true;
+	}
+
+	//
+	public void UnpauseTimer() {
+		// Add amount of time that's passed since timer was paused 
+		timeDone += (Time.time - timePaused);
+
+		// Unpause timer 
+		timeIsPaused = false;
+	}
 }
