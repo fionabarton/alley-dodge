5b269acd9fd9e6bb4343d96c66d34d86da891512
fionabarton <56236345+fionabarton@users.noreply.github.com>
2023-05-19 20:44:52 -0400

    On highlight, also increase UI element scale

--- UI/OnHighlightPopUpUIElement.cs
+++ UI/OnHighlightPopUpUIElement.cs
@@ -3,14 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-// On this UI object highlighted, "pop up" towards the player
+// On this UI object highlighted, "pop up" towards the player & increase its scale
 public class OnHighlightPopUpUIElement : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
     [Header("Set Dynamically")]
     RectTransform   rectTrans;
+
+    // Cached position values
     float           defaultZPos;
     float           popUpZPos;
 
-    void Start() {
+    // Cached scale values
+    Vector3         defaultScale;
+    Vector3         popUpScale;
+
+    void Awake() {
         rectTrans = GetComponent<RectTransform>();
     }
 
@@ -28,31 +34,43 @@ public class OnHighlightPopUpUIElement : MonoBehaviour, IPointerEnterHandler, IP
         // Remove event listener
         MoveUIMenus.onMoveMenusInOut -= SetFields;
 
-        // Set this UI object's z pos to default value
         if (Time.time > 0.5f) {
+            // Set z-pos to default value
             Vector3 pos = rectTrans.position;
             pos.z = defaultZPos;
             rectTrans.position = pos;
+
+            // Set scale to default value
+            rectTrans.localScale = defaultScale;
         }
     }
 
-    // Cache default and "pop up" z positions
+    // Cache default and "pop up" z position and scale values
     void SetFields() {
         defaultZPos = rectTrans.position.z;
         popUpZPos = defaultZPos - 0.15f;
+
+        defaultScale = rectTrans.localScale;
+        popUpScale = defaultScale * 1.1f;
     }
 
     public void OnPointerEnter(PointerEventData eventData) {
-        // Set this UI object's z pos to "pop up" value
+        // Set z-pos to "pop up" value
         Vector3 pos = rectTrans.position;
         pos.z = popUpZPos;
         rectTrans.position = pos;
+
+        // Set scale to "pop up" value
+        rectTrans.localScale = popUpScale;
     }
 
     public void OnPointerExit(PointerEventData eventData) {
-        // Set this UI object's z pos to default value
+        // Set z-pos to default value
         Vector3 pos = rectTrans.position;
         pos.z = defaultZPos;
         rectTrans.position = pos;
+
+        // Set scale to default value
+        rectTrans.localScale = defaultScale;
     }
 }
