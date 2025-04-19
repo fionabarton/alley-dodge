dbc975e2debcf92a20d74d187ab675a5c4d90684
fionabarton <56236345+fionabarton@users.noreply.github.com>
2025-04-18 22:18:45 -0500

    Update all code

--- Managers/ColorManager.cs
+++ Managers/ColorManager.cs
@@ -9,35 +9,59 @@ public class ColorManager : MonoBehaviour {
     public Material alleyMaterial1;
     public Material alleyMaterial2;
 
+    public Material playerMaterial1;
+    public Material playerMaterial2;
+
     [Header("Set dynamically")]
     // Current color palette index (0 = red & cyan)
     public int      colorNdx = 0;
 
-    List<Color> colors = new List<Color>() {
-        new Color(0.9f, 0.0f, 0.05f), // red
-        new Color(1.0f, 0.35f, 0.0f), // orange
-        new Color(1.0f, 0.8f, 0.0f), // yellow
-        new Color(0.5f, 1.0f, 0.0f), // chartreuse green
-        new Color(0.0f, 0.85f, 0.0f), // green
-        new Color(0.0f, 0.85f, 0.45f), // spring green
-        Color.cyan, // cyan
-        new Color(0.0f, 0.5f, 1.0f), // azure
-        Color.blue, // blue 
-        new Color(0.5f, 0.0f, 1.0f), // violet
-        Color.magenta, // magenta
-        new Color(1.0f, 0.0f, 0.5f) // rose
+    List<Color> alleyColors = new List<Color>() {
+        new Color(0.9f, 0, 0.05f, 1.0f), // red
+        new Color(1.0f, 0.35f, 0, 1.0f), // orange
+        new Color(1.0f, 0.8f, 0, 1.0f), // yellow
+        new Color(0.5f, 1.0f, 0, 1.0f), // chartreuse green
+        new Color(0, 0.85f, 0, 1.0f), // green
+        new Color(0, 0.85f, 0.45f, 1.0f), // spring green
+        new Color(0, 1, 1, 1.0f), // cyan 
+        new Color(0, 0.5f, 1.0f, 1.0f), // azure
+        new Color(0, 0, 1, 1.0f), // blue 
+        new Color(0.5f, 0, 1.0f, 1.0f), // violet
+        new Color(1, 0, 1, 1.0f), // magenta 
+        new Color(1.0f, 0, 0.5f, 1.0f) // rose
+    };
+
+    List<Color> playerColors = new List<Color>() {
+        new Color(0.9f, 0, 0.05f, 0.5f), // red
+        new Color(1.0f, 0.35f, 0, 0.5f), // orange
+        new Color(1.0f, 0.8f, 0, 0.5f), // yellow
+        new Color(0.5f, 1.0f, 0, 0.5f), // chartreuse green
+        new Color(0, 0.85f, 0, 0.5f), // green
+        new Color(0, 0.85f, 0.45f, 0.5f), // spring green
+        new Color(0, 1, 1, 0.5f), // cyan 
+        new Color(0, 0.5f, 1.0f, 0.5f), // azure
+        new Color(0, 0, 1, 0.5f), // blue 
+        new Color(0.5f, 0, 1.0f, 0.5f), // violet
+        new Color(1, 0, 1, 0.5f), // magenta 
+        new Color(1.0f, 0, 0.5f, 0.5f) // rose
     };
 
     public void Start() {
-        alleyMaterial1.color = colors[0];
-        alleyMaterial2.color = colors[6];
+        alleyMaterial1.color = alleyColors[0];
+        alleyMaterial2.color = alleyColors[6];
+
+        playerMaterial1.color = playerColors[0];
+        playerMaterial2.color = playerColors[6];
     }
 
     // Resets script properties to their default values
     public void ResetPalette() {
         colorNdx = 0;
-        alleyMaterial1.color = colors[0];
-        alleyMaterial2.color = colors[6];
+        alleyMaterial1.color = alleyColors[0];
+        alleyMaterial2.color = alleyColors[6];
+
+        playerMaterial1.color = playerColors[0];
+        playerMaterial2.color = playerColors[6];
     }
 
     // Sets UI display text colors to alley colors
@@ -53,20 +77,23 @@ public class ColorManager : MonoBehaviour {
     // Sets the two alley materials to a new set of two complementary colors 
     public void GetNewColorPalette() {
         // Increment colorNdx
-        if (colorNdx < colors.Count - 1) {
+        if (colorNdx < alleyColors.Count - 1) {
             colorNdx += 1;
         } else {
             colorNdx = 0;
         }
 
         // Set material 1 color
-        alleyMaterial1.color = colors[colorNdx];
+        alleyMaterial1.color = alleyColors[colorNdx];
+        playerMaterial1.color = playerColors[colorNdx];
 
         // Set material 2 color
         if (colorNdx < 6) {
-            alleyMaterial2.color = colors[colorNdx + 6];
+            alleyMaterial2.color = alleyColors[colorNdx + 6];
+            playerMaterial2.color = playerColors[colorNdx + 6];
         } else {
-            alleyMaterial2.color = colors[colorNdx - 6];
+            alleyMaterial2.color = alleyColors[colorNdx - 6];
+            playerMaterial2.color = playerColors[colorNdx - 6];
         }
     }
 }
--- Managers/ConfettiManager.cs
+++ Managers/ConfettiManager.cs
@@ -2,11 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// Confetti, fireworks, applause
 public class ConfettiManager : MonoBehaviour {
     [Header("Set in Inspector")]
     public List<ParticleSystem> confettiPS;
-
-    public ParticleSystem       fireworksPS;
+    public List<ParticleSystem> fireworksPS;
+    //public ParticleSystem       fireworksPS;
 
     // Plays particle systems that drop confetti for approximately 3 seconds
     public void DropConfetti(bool loopAudio = false, bool playFireworks = false) {
@@ -19,7 +20,10 @@ public class ConfettiManager : MonoBehaviour {
 
         // Play fireworks
         if (playFireworks) {
-            fireworksPS.Play();
+            //fireworksPS.Play();
+            for (int i = 0; i < fireworksPS.Count; i++) {
+                fireworksPS[i].Play();
+            }
         }
     }
 
@@ -30,12 +34,76 @@ public class ConfettiManager : MonoBehaviour {
             confettiMain.loop = isLooping;
         }
 
-        var fireworksMain = fireworksPS.main;
-        fireworksMain.loop = isLooping;
+        //var fireworksMain = fireworksPS.main;
+        //fireworksMain.loop = isLooping;
+        for (int i = 0; i < fireworksPS.Count; i++) {
+            var fireworksMain = fireworksPS[i].main;
+            fireworksMain.loop = isLooping;
+        }
 
         // Stop looping applause SFX
         if (!isLooping) {
             GameManager.audioMan.applauseSFXAudioSource.Stop();
         }
+    }
+
+    /////////////////////////////////////////////////////////////////////////
+
+    // Confetti
+    public void DoConfetti(bool enable) {
+        // Enable loop
+        for (int i = 0; i < confettiPS.Count; i++) {
+            var confettiMain = confettiPS[i].main;
+            confettiMain.loop = enable;
+        }
+
+        // Play confetti
+        if (enable) {
+            for (int i = 0; i < confettiPS.Count; i++) {
+                confettiPS[i].Play();
+            }
+        }
+    }
+
+    // Fireworks
+    public void DoFireworks(bool enable) {
+        //// Enable loop
+        //var fireworksMain = fireworksPS.main;
+        //fireworksMain.loop = enable;
+
+        //// Play fireworks
+        //if (enable) {
+        //    fireworksPS.Play();
+        //}
+        // Enable loop
+        for (int i = 0; i < fireworksPS.Count; i++) {
+            var fireworksMain = fireworksPS[i].main;
+            fireworksMain.loop = enable;
+        }
+
+        // Play fireworks
+        if (enable) {
+            for (int i = 0; i < fireworksPS.Count; i++) {
+                fireworksPS[i].Play();
+            }
+        }
+    }
+
+    // Applause
+    public void DoApplause(bool enable) {
+        if (enable) {
+            // Play SFX
+            GameManager.audioMan.PlayApplauseSFXlip();
+        } else {
+            // Stop SFX
+            GameManager.audioMan.applauseSFXAudioSource.Stop();
+        }
+    }
+
+    // All
+    public void DoAll(bool enable) {
+        DoConfetti(enable);
+        DoFireworks(enable);
+        DoApplause(enable);
     }
 }
